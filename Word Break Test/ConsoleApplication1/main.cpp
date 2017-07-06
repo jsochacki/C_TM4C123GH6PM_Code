56250d53a54ada527a5212d9235dbdf723ea9d99
Mason Edgar <Mason.Edgar@viasat.com>
2017-07-06 16:01:13 -0700

    Preface Register API
    Setup the API to program the Preface Registers

--- Word Break Test/ConsoleApplication1/main.cpp
+++ Word Break Test/ConsoleApplication1/main.cpp
@@ -5,13 +5,16 @@
 #define LOWER_8_BITS 0x000000FF
 #define MIDDLE_8_BITS 0X0000FF00
 #define UPPER_8_BITS 0x00FF0000
+#define temp 0x987654
+
+void foo(unsigned long word);
 
 int main(void)
 {
 
-	unsigned long temp = 0x128565; 
+	//unsigned long temp = 0x128565; 
 
-	
+	/*
 	unsigned long num1 = temp & UPPER_8_BITS;
 	unsigned long num2 = temp & LOWER_8_BITS;
 	unsigned long num3 = temp & MIDDLE_8_BITS; 
@@ -24,14 +27,24 @@ int main(void)
 
 
 	printf("upper: %x\n\nmiddle: %x\n\nlower: %x\n\n", upper, middle, lower);
+	*/
 
-
-
-
-
-
-
-
+	foo(temp); 
 
 	return 0;
+}
+
+
+void foo(unsigned long word) {
+
+	unsigned long num1 = word & UPPER_8_BITS;
+	unsigned long num2 = word & LOWER_8_BITS;
+	unsigned long num3 = word & MIDDLE_8_BITS;
+
+	unsigned short upper = num1 >> 16;
+	unsigned short middle = num3 >> 8;
+	unsigned short lower = num2;
+
+	printf("upper: %x\n\nmiddle: %x\n\nlower: %x\n\n", upper, middle, lower);
+
 }
